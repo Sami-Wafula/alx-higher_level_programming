771b541f7a7eb8412e8aead6e411159167f14787
Sami-Wafula <samiwafula3257@gmail.com>
2022-05-31 05:14:15 -0700

    alphabet-edit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for letter in range(97,100:102,112:114,123):
-    print("{}".format(chr(letter)), end="")
+for letter in range(97, 123):
+        if chr(letter) is not 'q' and chr(letter) is not 'e':
+                    print("{}".format(chr(letter)), end="")
