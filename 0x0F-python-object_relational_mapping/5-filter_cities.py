afcdf8116b0b002bafc375cd35f59c537c2f7c24
Sami-Wafula <samiwafula3257@gmail.com>
2022-09-11 07:46:09 +0300

    fix syntax

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -9,19 +9,19 @@ if __name__ == '__main__':
     if len(sys.argv) >= 5:
         db_connection = b.connect(
             host='localhost',
-            username=sys.argv[1],
+            user=sys.argv[1],
             password=sys.argv[2],
             db=sys.argv[3],
         )
         cursor = db_connection.cursor()
-        state = sys.argv[4]
+        state_name = sys.argv[4]
         cursor.execute(
             'SELECT cities.name FROM cities' +
             ' INNER JOIN states ON cities.state_id = states.id' +
             ' WHERE CAST(states.name AS BINARY) = %s' +
             ' ORDER BY cities.id ASC;',
-            [state]
-            )
+            [state_name]
+        )
         results = cursor.fetchall()
         for result in results:
             print(result)
